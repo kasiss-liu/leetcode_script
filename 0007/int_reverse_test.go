4e4f2889f34286217e4c77bc2a66c558e8385e69
kasiss <kasiss@163.com>
2020-03-19 11:29:10 +0800

    优化算法

--- 0007/int_reverse_test.go
+++ 0007/int_reverse_test.go
@@ -1,41 +1,21 @@
 package main
 
 import (
-	"fmt"
-	"math"
 	"testing"
 )
 
 func reverse(x int) int {
-	sybol := 1
-	if x < 0 {
-		sybol = -1
-		x = x * sybol
-	}
-
-	ints := make([]int, 0, 10)
-
-	for {
-		if x <= 0 {
-			break
+	rint := 0
+	minInt32 := -(1 << 31)
+	maxInt32 := (1 << 31) - 1
+	for x != 0 {
+		rint = rint*10 + x%10
+		if rint > maxInt32 || rint < minInt32 {
+			return 0
 		}
-		c := x % 10
-		ints = append(ints, c)
-		x = (x - c) / 10
+		x /= 10
 	}
-	var rint int
-	l := len(ints)
-	p := 1
-	for i := l - 1; i >= 0; i-- {
-		rint = rint + ints[i]*p
-		p = p * 10
-	}
-
-	if rint > math.MaxInt32 {
-		rint = 0
-	}
-
-	return rint * sybol
+	return rint
 }
 
 func TestIntReverse(t *testing.T) {
