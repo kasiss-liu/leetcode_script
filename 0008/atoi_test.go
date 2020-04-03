7933946667b6708833ad0c0bb66054bb2f75a9c3
liuxiang17 <liuxiang17@58.com>
2020-04-03 12:32:58 +0800

    整数转换

---
+++ 0008/atoi_test.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"testing"
+)
+
+func myAtoi(s string) int {
+	var maxInt = 1<<31 - 1
+	var minInt = -1 << 31
+	var number = 0
+	var symbol = 1
+	var first bool = true
+	for _, b := range s {
+		//非空字符跳过
+		if first && b == 32 {
+			continue
+		}
+		//第一个有效字符是符号
+		if first && (b == 45 || b == 43) {
+			if b == 45 {
+				symbol = -1
+			}
+			first = false
+			continue
+		}
+		//有效int
+		if b >= 48 && b <= 57 {
+			if first {
+				first = false
+			}
+			number = number*10 + int(b) - 48
+
+			if number*symbol > maxInt {
+				return maxInt
+			}
+			if number*symbol < minInt {
+				return minInt
+			}
+			continue
+		}
+		//非有效int结束匹配
+		if first {
+			number = 0
+		}
+		break
+	}
+
+	return number * symbol
+}
+
+func TestMyAtoi(t *testing.T) {
+	var s string
+	var exp, res int
+
+	s = "   -42"
+	exp = -42
+	res = myAtoi(s)
+	if exp != res {
+		t.Error(exp, "not equal", res)
+	}
+
+	s = "-91283472332"
+	exp = -2147483648
+	res = myAtoi(s)
+	if exp != res {
+		t.Error(exp, "not equal", res)
+	}
+
+	s = "words and 987"
+	exp = 0
+	res = myAtoi(s)
+	if exp != res {
+		t.Error(exp, "not equal", res)
+	}
+
+	s = "9223372036854775808"
+	exp = 2147483647
+	res = myAtoi(s)
+	if exp != res {
+		t.Error(exp, "not equal", res)
+	}
+}
